b13713a510db57c3add3d5c96c72ed80f1db50b4
marryem21 <maryem-hamdi@student.kirkwood.edu>
2022-07-07 16:12:02 +0100

    final

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
@@ -52,4 +52,41 @@ $('.carousel').owlCarousel({
         }
     }
 });   
+
+//roadmap
+// var mySwiper = new Swiper(".swiper", {
+//     autoHeight: true,
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false
+//     },
+//     speed: 500,
+//     direction: "horizontal",
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev"
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         type: "progressbar"
+//     },
+//     loop: false,
+//     effect: "slide",
+//     spaceBetween: 30,
+//     on: {
+//         init: function () {
+//             $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+//             $(".swiper-pagination-custom .swiper-pagination-switch").eq(0).addClass("active");
+//         },
+//         slideChangeTransitionStart: function () {
+//             $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+//             $(".swiper-pagination-custom .swiper-pagination-switch").eq(mySwiper.realIndex).addClass("active");
+//         }
+//     }
+// });
+// $(".swiper-pagination-custom .swiper-pagination-switch").click(function () {
+//     mySwiper.slideTo($(this).index());
+//     $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+//     $(this).addClass("active");
+// });
 });
