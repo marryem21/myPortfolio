13f606d45abc2b929b8002451a3729cfaf75c25a
marryem21 <maryem-hamdi@student.kirkwood.edu>
2022-07-25 20:40:30 +0100

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- $(document).ready(function(){
+$(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
@@ -11,6 +11,26 @@
             $('.scroll-up-btn').removeClass("show");
         }
     });
+/*test */
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}  
+window.addEventListener("scroll", reveal);
+
+// test ends
+
+
 
 //slide-up script
 $('.scroll-up-btn').click(function(){
@@ -53,40 +73,4 @@ $('.carousel').owlCarousel({
     }
 });   
 
-//roadmap
-// var mySwiper = new Swiper(".swiper", {
-//     autoHeight: true,
-//     autoplay: {
-//         delay: 5000,
-//         disableOnInteraction: false
-//     },
-//     speed: 500,
-//     direction: "horizontal",
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev"
-//     },
-//     pagination: {
-//         el: ".swiper-pagination",
-//         type: "progressbar"
-//     },
-//     loop: false,
-//     effect: "slide",
-//     spaceBetween: 30,
-//     on: {
-//         init: function () {
-//             $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
-//             $(".swiper-pagination-custom .swiper-pagination-switch").eq(0).addClass("active");
-//         },
-//         slideChangeTransitionStart: function () {
-//             $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
-//             $(".swiper-pagination-custom .swiper-pagination-switch").eq(mySwiper.realIndex).addClass("active");
-//         }
-//     }
-// });
-// $(".swiper-pagination-custom .swiper-pagination-switch").click(function () {
-//     mySwiper.slideTo($(this).index());
-//     $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
-//     $(this).addClass("active");
-// });
 });
